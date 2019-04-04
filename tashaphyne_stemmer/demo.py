fd61fd9c39289d563ee395d7ad8ea13cabe5ef5e
Hanene Maafi <fh_maafi@esi.dz>
2019-04-04 14:35:43 +0100

    fix: test_words fils format

--- tashaphyne_stemmer/demo.py
+++ tashaphyne_stemmer/demo.py
@@ -1,12 +1,12 @@
 import tashaphyneStemmer
 
-string = 'مكتبة لمعالجة الكلمات العربية  وتجذيعها'
+string = u'مكتبة لمعالجة الكلمات العربية  وتجذيعها'
 
 stems_dict = tashaphyneStemmer.stem(string)
 
 file = open('test_words.txt', 'w+', encoding="utf-8")
 
 for word_count in stems_dict:
-    file.write(word_count + ": "+stems_dict[word_count]+'\n')
+    file.write(stems_dict[word_count]+'\n')
 
 file.close()
