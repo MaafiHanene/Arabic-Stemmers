cbf12f7b6612b260653a74b6559ce8f7db9ba258
Hanene Maafi <fh_maafi@esi.dz>
2019-04-06 21:10:02 +0100

    feat: Arabic-Stemming-Toolkit [Algo3]

---
+++ arabic_stemming_toolkit/ast-algo3/demo.py
@@ -0,0 +1,12 @@
+from arabicStemmingToolkitAlgo3 import ArabicStemmingToolkitStemmerAlgo3 as ast_algo3_stemmer
+
+string = u'مكتبة لمعالجة الكلمات العربية  وتجذيعها'
+
+stems_list = ast_algo3_stemmer.stem(ast_algo3_stemmer, string)
+
+file = open('output.txt', 'w+', encoding="utf-8")
+
+for word in stems_list:
+    file.write(word+'\n')
+
+file.close()
